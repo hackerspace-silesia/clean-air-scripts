974f315556d28f2e4c3dda7eaa4e687af67d6105
Firemark <marpiechula@gmail.com>
2019-07-03 00:00:19 +0200

    upgrade gios script

--- gios.py
+++ gios.py
@@ -1,21 +1,20 @@
 # coding=utf-8
 
-import ast
 import json
 import os
 import urllib
 from urllib.request import Request
-from logging import getLogger
+from collections import defaultdict
+from time import time
 
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-LOGGER = getLogger('gios')
+URL = 'http://api.gios.gov.pl/pjp-api/rest/'
 
 
 def all_data():
-    url = "http://api.gios.gov.pl/pjp-api/rest/station/findAll"
+    url = URL + "station/findAll"
 
     if url.lower().startswith('http'):
         gios = Request(url)
@@ -28,147 +27,97 @@ def all_data():
     return gios_all_stations
 
 
-def gios():
-    pm25_count = 0  # How many pm2,5 metrics were found totaly
-    pm10_count = 0  # How many pm10 metrics were found totaly
-    pm25_missing_count = 0  # How many pm2,5 metrics were found empty
-    pm10_missing_count = 0  # How many pm10 metrics were found empty
-    result_ids = []
-    result_latitude = []
-    result_longitude = []
-    lat_pm10 = []
-    long_pm10 = []
-    pm10 = []
-    lat_pm25 = []
-    long_pm25 = []
-    pm25 = []
-    final_list_of_measurements_in_dictionary = []
+class ParseError(Exception):
 
-    gios_all_stations = all_data()
-    LOGGER.debug("gios_all_stations %s", gios_all_stations)
+    def __init__(self, *args, sensor_type):
+        self.sensor_type = sensor_type
+        super().__init__(*args)
+
+
+def _get_data_from_gios_station(sensor_index, gios_sensor_data):
+    sensor_type = gios_sensor_data['param']['paramFormula']
+    if sensor_type not in {'PM10', 'PM2.5'}:
+        return None
+
+    sensor_id = gios_sensor_data['id']
+    station_id_data = Request(URL + f"/data/getData/{sensor_id}")
+    station_id = gios_sensor_data['stationId']
+    station_sensor_data = urllib.request.urlopen(station_id_data, timeout=60)
+    station_sensor_data = json.loads(station_sensor_data.read())
 
     try:
-        result_ids = [ids['id'] for ids in gios_all_stations]
-        LOGGER.debug("result_ids %s", result_ids)
+        first_sensor = next(
+            val for val in station_sensor_data['values']
+            if val['value'] is not None
+        )
+    except StopIteration:
+        raise ParseError(
+            f'empty not-none values, i={sensor_index}, station_id={station_id}', 
+            sensor_type=sensor_type,
+        )
 
-        result_latitude = [latitude['gegrLat'] for latitude in gios_all_stations]
-        LOGGER.debug("result_latitude %s", result_latitude)
+    try:
+        value = first_sensor['value']
+        value = float(value)
+    except TypeError:
+        raise ParseError(
+            f'wrong value={value}, i={sensor_index}, station_id={station_id}', 
+            sensor_type=sensor_type,
+        )
 
-        result_longitude = [longitude['gegrLon'] for longitude in gios_all_stations]
-        LOGGER.debug("result_longitude %s", result_longitude)
+    return sensor_type, value
 
-    except (KeyError, ValueError):
-        LOGGER.debug("Can't read gios data")
 
-    merged_ids_lat_long = list(zip(result_ids, result_latitude, result_longitude))
+def gios():
+    t_global = time()
+    errors_counter = defaultdict(int)
+    values = []
 
-    total_values = len(merged_ids_lat_long)
-    for i, values in enumerate(merged_ids_lat_long):
-        sensor_url = Request('http://api.gios.gov.pl/pjp-api/rest/station/sensors/' + str(values[0]))
+    gios_all_stations = all_data()
+
+    total_values = len(gios_all_stations)
+    for i, obj in enumerate(gios_all_stations):
+        t = time()
+        print(i, '/', total_values, '...', end='')
+        result_id = obj['id']
+        sensor_url = Request(URL + f'station/sensors/{result_id}')
         gios_sensor_data = urllib.request.urlopen(sensor_url, timeout=60)
-        print(values, i, '/', total_values, '...')
+        gios_sensor_data = json.loads(gios_sensor_data.read())
 
-        try:
-            gios_sensor_data = json.loads(gios_sensor_data.read())
-        except TypeError:
-            gios_sensor_data = ast.literal_eval(gios_sensor_data)
-        except (ValueError, IndexError, KeyError, TypeError):
-            gios_sensor_data = 2
-
-        for i in (range(10)):
-            LOGGER.debug("Trying %s", 1)
-
+        for sensor_index, gios_sensor_single_data in enumerate(gios_sensor_data):
             try:
-
-                param_formula = str(gios_sensor_data[i]['param']['paramFormula'])
-                sensor_id = gios_sensor_data[i]['id']
-                if param_formula == str("PM10"):
-                    pm10_count += 1
-                    LOGGER.debug("Param formula %s", param_formula)
-                    station_id_data = Request(f"http://api.gios.gov.pl/pjp-api/rest/data/getData/{sensor_id}")
-                    station_sensor_data = urllib.request.urlopen(station_id_data, timeout=60)
-                    station_sensor_data = json.loads(station_sensor_data.read())
-                    LOGGER.debug("Param station_sensor_data %s", station_sensor_data)
-
-                    try:
-                        pm10_value = station_sensor_data['values'][0]['value']
-                        pm10_value = float(pm10_value)
-                        LOGGER.debug("PM10_value %s", pm10_value)
-
-                    except (TypeError, IndexError):
-                        pm10_missing_count += 1
-                        continue
-
-                    pm10.append(pm10_value)
-                    lat_pm10.append(values[1])
-                    long_pm10.append(values[2])
-
-                    LOGGER.debug("pm10, lat_pm10, long_pm10 %s %s %s ", pm10, lat_pm10, long_pm10)
-
-                elif str(param_formula) == str("PM2.5"):
-                    pm25_count += 1
-                    LOGGER.debug("Param formula %s", param_formula)
-
-                    station_id_data = Request(f"http://api.gios.gov.pl/pjp-api/rest/data/getData/{sensor_id}")
-                    station_sensor_data = urllib.request.urlopen(station_id_data, timeout=60)
-                    station_sensor_data = json.loads(station_sensor_data.read())
-                    LOGGER.debug("Param station_sensor_data %s", station_sensor_data)
-
-                    try:
-                        pm25_value = station_sensor_data['values'][0]['value']
-                        pm25_value = float(pm25_value)
-                        LOGGER.debug("pm25_value %s", pm25_value)
-
-                    except (TypeError, IndexError):
-                        pm25_missing_count += 1
-                        continue
-
-                    pm25.append(pm25_value)
-                    lat_pm25.append(values[1])
-                    long_pm25.append(values[2])
-
-                    LOGGER.debug("pm25, lat_pm25, long_pm25 %s %s %s ", pm25, lat_pm25, long_pm25)
-
-            except:
+                useful_data = _get_data_from_gios_station(
+                    sensor_index=sensor_index,
+                    gios_sensor_data=gios_sensor_single_data,
+                )
+            except ParseError as error:
+                errors_counter[error.sensor_type] += 1
+                print('!!!', error)
                 continue
 
-    print(f"PM25 missing count {pm25_missing_count}\n"
-          f"PM25 total count {pm25_count}\n"
-          f"PM25 sum of valid measurements {pm25_count - pm25_missing_count}\n"
-          f"PM10 missing count {pm10_missing_count}\n"
-          f"PM10 total count {pm10_count}\n"
-          f"PM10 sum of valid measurements {pm10_count - pm10_missing_count}\n")
+            if useful_data is None:
+                continue
+            sensor_type, value = useful_data
+            values.append({
+                'sensor_type': sensor_type,
+                'sensor': 'WIOS',
+                'value': value,
+                'id': obj['id'],
+                'lat': obj['gegrLat'],
+                'lon': obj['gegrLon'],
+            })
 
-    merged_ids_lat_long_pm10 = list(zip(lat_pm10, long_pm10, pm10))
-    LOGGER.debug("merged_ids_lat_long_pm10 %s ", merged_ids_lat_long_pm10)
+        print(' time: %.2fs' % (time() - t))
 
-    merged_ids_lat_long_pm25 = list(zip(lat_pm25, long_pm25, pm25))
-    LOGGER.debug("merged_ids_lat_long_pm25 %s ", merged_ids_lat_long_pm25)
+    counter_by_sensor = defaultdict(int)
+    for obj in values:
+        counter_by_sensor[obj['sensor_type']] += 1
 
-    for values in merged_ids_lat_long_pm10:
-        data = {
-            "lat": str(values[0]),
-            "long": str(values[1]),
-            "pm10": float('%.2f' % values[2]),
-            "sensor": "WIOS"
-        }
-        LOGGER.debug("data %s", data)
-        final_list_of_measurements_in_dictionary.append(data)
-
-    for values in merged_ids_lat_long_pm25:
-        data = {
-            "lat": str(values[0]),
-            "long": str(values[1]),
-            "pm25": float('%.2f' % values[2]),
-            "sensor": "WIOS"
-        }
-        LOGGER.debug("data %s", data)
-        final_list_of_measurements_in_dictionary.append(data)
-
-    LOGGER.debug("final_list_of_measurements_in_dictionary %s", final_list_of_measurements_in_dictionary)
-
-    import pprint as p; p.pprint(final_list_of_measurements_in_dictionary)
-
+    print('errors', errors_counter)
+    print('count by sensor type', counter_by_sensor)
+    print('total time: %.2fs' % (time() - t))
+    return values
 
 if __name__ == "__main__":
-    gios()
+    from pprint import pprint
+    pprint(gios())
