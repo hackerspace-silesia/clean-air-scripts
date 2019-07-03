b73cabab696fecc03701206de2859c463757c42b
Firemark <marpiechula@gmail.com>
2019-07-03 22:31:08 +0200

    refactoring lufdaten script

--- luftdaten.py
+++ luftdaten.py
@@ -10,35 +10,9 @@ from urllib3.util import Retry
 from logging import getLogger
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-LOGGER = getLogger('lufdaten')
 
 
-def _requests_retry_session(
-        retries=3, back_off_factor=3, status_force_list=(500, 502, 504), session=None
-):
-    """
-    Function will help with exponential back-off when calling BI API
-    :param retries: int
-    :param back_off_factor: int
-    :param status_force_list: list http codes returned by api
-    :param session: None
-    :return: session
-    """
-    session = session or requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=back_off_factor,
-        status_forcelist=status_force_list,
-    )
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount("http://", adapter)
-    session.mount("https://", adapter)
-    return session
-
-
-def all_data():
+def get_all_data():
     url = "http://api.luftdaten.info/v1/filter/country=PL"
 
     if url.lower().startswith("http"):
@@ -49,82 +23,53 @@ def all_data():
     with urllib.request.urlopen(luftdaten, timeout=60) as resp:
         luftdaten_all_stations = json.loads(resp.read())
 
-    LOGGER.debug("Luftdaten all stations %s", luftdaten_all_stations)
     for data in luftdaten_all_stations:
         yield data
 
 
-def luftdaten():
-    sensor_name_list = []
-    sensor_lat_list = []
-    sensor_long_list = []
-    pm10_value = ""
-    pm25_value = ""
-    final_list_of_measurements_in_dictionary = []
-    luftdaten_all_data = all_data()
+def to_float(x):
     try:
-        for data in luftdaten_all_data:
-            sensor_name = data["sensor"]["sensor_type"]["name"]
-            sensor_name_list.append(sensor_name)
+        return float(x)
+    except ValueError:
+        return None
 
-            sensor_lat = data["location"]["latitude"]
-            sensor_lat_list.append(sensor_lat)
 
-            sensor_long = data["location"]["longitude"]
-            sensor_long_list.append(sensor_long)
+def _get_fancy_data_from_single_luftdaten(data):
+    sensor_name = data["sensor"]["sensor_type"]["name"]
+    if sensor_name != "SDS011":
+        return None
 
-            LOGGER.debug(
-                "Sensor name: %s, Lat: %s, Long: %s",
-                sensor_name,
-                sensor_lat,
-                sensor_long,
-            )
+    sensor_values = {
+        sensor_data['value_type']: to_float(sensor_data.get('value'))
+        for sensor_data in data["sensordatavalues"]
+    }
 
-            for _ in data["sensordatavalues"]:
-                value_type = _.get("value_type")
-                if value_type == "P1":
-                    pm10_value = _.get("value")
-                    LOGGER.debug(
-                        "PM10: %s, Sensor name: %s, Lat: %s, Long: %s",
-                        pm10_value,
-                        sensor_name,
-                        sensor_lat,
-                        sensor_long,
-                    )
+    sensor_lat = data["location"]["latitude"]
+    sensor_long = data["location"]["longitude"]
+    return {
+        "lat": sensor_lat,
+        "long": sensor_long,
+        "pm25": sensor_values.get('P2'),
+        "pm10": sensor_values.get('P1'),
+        "sensor": sensor_name,
+    }
 
-                elif value_type == "P2":
-                    pm25_value = _.get("value")
-                    LOGGER.debug(
-                        "PM25: %s, Sensor name: %s, Lat: %s, Long: %s",
-                        pm25_value,
-                        sensor_name,
-                        sensor_lat,
-                        sensor_long,
-                    )
 
-            if sensor_name == "SDS011":
-                data = {
-                    "lat": sensor_lat,
-                    "long": sensor_long,
-                    "pm25": pm25_value,
-                    "pm10": pm10_value,
-                    "sensor": sensor_name,
-                }
-
-                LOGGER.debug("data %s", data)
-                final_list_of_measurements_in_dictionary.append(data)
-
+def luftdaten():
+    try:
+        luftdaten_all_data = get_all_data()
     except urllib.error.HTTPError as e:
-        LOGGER.critical("luftdaten returned %s", e)
+        print("luftdaten returned %s" % e)
 
-    LOGGER.debug(
-        "final_list_of_measurements_in_dictionary %s",
-        final_list_of_measurements_in_dictionary,
+    parsed_data = (
+        _get_fancy_data_from_single_luftdaten(data)
+        for data in luftdaten_all_data
     )
 
-    from pprint import pprint
-    pprint(final_list_of_measurements_in_dictionary)
+    filtered_data = [data for data in parsed_data if data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    luftdaten()
+    from pprint import pprint
+    pprint(luftdaten())
